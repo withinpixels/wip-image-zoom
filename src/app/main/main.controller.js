5b7e3ef1a1ed6ca28ca72173fe4df3ba619bc842
mustafahlvc <mustafahlvc@gmail.com>
2016-11-03 11:24:48 +0300

    extra slashes removed from img paths

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -27,39 +27,39 @@
             thumbColPadding: 4,
             images         : [
                 {
-                    thumb : '/assets/images/1-thumb.jpg',
-                    medium: '/assets/images/1-medium.jpg',
-                    large : '/assets/images/1-large.jpg'
+                    thumb : 'assets/images/1-thumb.jpg',
+                    medium: 'assets/images/1-medium.jpg',
+                    large : 'assets/images/1-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/2-thumb.jpg',
-                    medium: '/assets/images/2-medium.jpg',
-                    large : '/assets/images/2-large.jpg'
+                    thumb : 'assets/images/2-thumb.jpg',
+                    medium: 'assets/images/2-medium.jpg',
+                    large : 'assets/images/2-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/3-thumb.jpg',
-                    medium: '/assets/images/3-medium.jpg',
-                    large : '/assets/images/3-large.jpg'
+                    thumb : 'assets/images/3-thumb.jpg',
+                    medium: 'assets/images/3-medium.jpg',
+                    large : 'assets/images/3-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/4-thumb.jpg',
-                    medium: '/assets/images/4-medium.jpg',
-                    large : '/assets/images/4-large.jpg'
+                    thumb : 'assets/images/4-thumb.jpg',
+                    medium: 'assets/images/4-medium.jpg',
+                    large : 'assets/images/4-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/5-thumb.jpg',
-                    medium: '/assets/images/5-medium.jpg',
-                    large : '/assets/images/5-large.jpg'
+                    thumb : 'assets/images/5-thumb.jpg',
+                    medium: 'assets/images/5-medium.jpg',
+                    large : 'assets/images/5-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/6-thumb.jpg',
-                    medium: '/assets/images/6-medium.jpg',
-                    large : '/assets/images/6-large.jpg'
+                    thumb : 'assets/images/6-thumb.jpg',
+                    medium: 'assets/images/6-medium.jpg',
+                    large : 'assets/images/6-large.jpg'
                 },
                 {
-                    thumb : '/assets/images/7-thumb.jpg',
-                    medium: '/assets/images/7-medium.jpg',
-                    large : '/assets/images/7-large.jpg'
+                    thumb : 'assets/images/7-thumb.jpg',
+                    medium: 'assets/images/7-medium.jpg',
+                    large : 'assets/images/7-large.jpg'
                 }
             ]
         };
